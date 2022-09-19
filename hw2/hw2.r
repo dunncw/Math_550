2c7593a33158323a482971dd153a755adf16f87e
dunncw <dunncw@g.cofc.edu>
2022-09-19 13:36:24 -0400

    hw2

---
+++ hw2/hw2.r
@@ -0,0 +1,23 @@
+##not my work credit to https://jolars.github.io/MARsolutions/diagnostics-and-transformations-for-simple-linear-regressions.html
+
+# 3.4 Tryfos 1988
+# a (a) Does the straight line regression model (3.8) seem to fit the data well? If not, list any weaknesses apparent in model (3.8).
+#answer
+# The line is not a good fit for the data as the variance of the data increases as the tonnage of the boat increases. 
+# Lastly the distribution of the residuals seems non-random. Also there seems to be two outlier points.
+
+
+# b (b) Suppose that model (3.8) was used to calculate a prediction interval for Time when Tonnage = 10,000. Would the interval be too short,
+# too long or about right (i.e., valid)? Give a reason to support your answer.
+# answer
+# The data seems to have two clusters and the point prediction interval is right inbetween these two clusters. 
+# The prediction interval would probably be too short or perhaps even valid. We expect the intervals for the model to be too high for high tonnages and too low for low tonnages.
+
+# c
+# It appears to be something of an improvement. The residuals appear “more” random. Prediction intervals are likely to be more valid for this model because of the more random residuals.
+
+# d
+# The residuals’ variance decreases for higher tonnages. The distribution of the residuals is heavy-tailed.
+
+#q8
+
