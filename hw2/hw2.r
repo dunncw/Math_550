bec2d1a3b2960f219dee8b937567dfc7c3e3812c
CaydenD <caydendunn2121@gmail.com>
2022-09-19 14:27:21 -0400

    problem with package carret on pc

--- hw2/hw2.r
+++ hw2/hw2.r
@@ -1,23 +1,23 @@
-##not my work credit to https://jolars.github.io/MARsolutions/diagnostics-and-transformations-for-simple-linear-regressions.html
+#8
+# Part 1a
+diamond_data <- read.delim(file.path("C:/Users/cayde/Desktop/grad_school_code/Math550/Math_550/hw2/data/diamonds.txt"))
 
-# 3.4 Tryfos 1988
-# a (a) Does the straight line regression model (3.8) seem to fit the data well? If not, list any weaknesses apparent in model (3.8).
-#answer
-# The line is not a good fit for the data as the variance of the data increases as the tonnage of the boat increases. 
-# Lastly the distribution of the residuals seems non-random. Also there seems to be two outlier points.
+slr = lm(Price~Size, data=diamond_data)
+summary(slr)
 
+#adj R^2 val is 97.8% so this model is perfect for predicting the price of diamonds using only size
 
-# b (b) Suppose that model (3.8) was used to calculate a prediction interval for Time when Tonnage = 10,000. Would the interval be too short,
-# too long or about right (i.e., valid)? Give a reason to support your answer.
-# answer
-# The data seems to have two clusters and the point prediction interval is right inbetween these two clusters. 
-# The prediction interval would probably be too short or perhaps even valid. We expect the intervals for the model to be too high for high tonnages and too low for low tonnages.
+# Part 1b
+slr
 
-# c
-# It appears to be something of an improvement. The residuals appear “more” random. Prediction intervals are likely to be more valid for this model because of the more random residuals.
+# Part 2a
+library(caret)
+feature_engineered_dd <- diamond_data
+feature_engineered_dd$Size <- (feature_engineered_dd$Size)
+feature_engineered_dd$Price <- preProcess(feature_engineered_dd$Price, method = c('range'))
 
-# d
-# The residuals’ variance decreases for higher tonnages. The distribution of the residuals is heavy-tailed.
+slr = lm(Price~., data=feature_engineered_dd)
+summary(slr)
 
-#q8
-
+# Part 2b
+slr
